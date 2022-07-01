5a7425b002d31a35dd41c8c714f75afd80214cd9
alessandrobasi <13107787+alessandrobasi@users.noreply.github.com>
2022-07-01 14:51:19 +0200

    update

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ from shutil import rmtree
 from PyQt5.QtCore import Qt
 
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -27,12 +26,12 @@ class MainWindow(QMainWindow):
         self.show()
         if self.__nextFunct != "":
             self.__nextFunct()
-    
+
     def getFile(self, local=None):
         if not os.path.exists("cache"):
             os.mkdir("cache")
-        
-        if local and ( not os.path.exists(local) or (time() - os.path.getmtime(local)) > 604800):
+
+        if local and (not os.path.exists(local) or (time() - os.path.getmtime(local)) > 604800):
             r = requests.get("https://badges-content.teamspeak.com/list")
             if r.status_code == 200:
                 with open("list", "wb") as f:
@@ -40,7 +39,7 @@ class MainWindow(QMainWindow):
 
         with open("list", "r", encoding="ansi") as f:
             text = f.read()
-        regex = r"^\$(?P<headid>[\w\d-]+)\x12(.|\n)(?P<nome>.+)\x1A.(?P<url>https://[\w\-\.\/]+)\x22\W(?P<desc>\w.*)\(.+$"
+        regex = r"^\$(?P<headid>[\w\d-]+).(.|\n)(?P<nome>.+)..(?P<url>https://[\w\-\.\/]+)..(?P<desc>\w.*)\(.+$"
         matches = re.finditer(regex, text, re.UNICODE | re.MULTILINE)
         for _, match in enumerate(matches, start=1):
             #print([match.group(i) for i in [1, 3, 4, 5]])
@@ -48,7 +47,7 @@ class MainWindow(QMainWindow):
             # 3: Nome
             # 4: url
             # 5: desc (ANSI) TODO: portarlo a utf-8
-            self.badges.append([str(match.group(i)) for i in [1, 3, 4, 5]] )
+            self.badges.append([str(match.group(i)) for i in [1, 3, 4, 5]])
 
     def getBadgeIcon(self, codeid, url, tipo="_64"):
         if not os.path.isfile("cache/"+codeid+tipo+".png"):
@@ -59,11 +58,12 @@ class MainWindow(QMainWindow):
                     f.write(r.content)
             else:
                 return ""
-        
+
         return "cache/"+codeid+tipo+".png"
 
     def showBadgesInfo(self):
-        uuid, nome, url, desc = self.tabellaBages.selectedItems()[0].data(Qt.UserRole)
+        uuid, nome, url, desc = self.tabellaBages.selectedItems()[
+            0].data(Qt.UserRole)
         self.iconaBadge.setPixmap(QPixmap(self.getBadgeIcon(uuid, url, "_64")))
         self.nomeBadge.setText(nome)
         self.descBadge.setText(desc)
@@ -71,9 +71,12 @@ class MainWindow(QMainWindow):
         self.uuidText.setText(uuid)
 
         self.svgUrl.setText("SVG: <a href='"+url+".svg"+"'>"+url+".svg"+"</a>")
-        self.detailsSvgUrl.setText("Details SVG: <a href='"+url+"_details.svg"+"'>"+url+"_details.svg"+"</a>")
-        self.detailsPngUrl.setText("Details PNG: <a href='"+url+"_64.png"+"'>"+url+"_64.png"+"</a>")
-        self.pngUrl.setText("PNG: <a href='"+url+"_16.png"+"'>"+url+"_16.png"+"</a>")
+        self.detailsSvgUrl.setText(
+            "Details SVG: <a href='"+url+"_details.svg"+"'>"+url+"_details.svg"+"</a>")
+        self.detailsPngUrl.setText(
+            "Details PNG: <a href='"+url+"_64.png"+"'>"+url+"_64.png"+"</a>")
+        self.pngUrl.setText("PNG: <a href='"+url +
+                            "_16.png"+"'>"+url+"_16.png"+"</a>")
 
     def clearCache(self):
         os.remove("list")
@@ -92,11 +95,11 @@ class MainWindow(QMainWindow):
         # 3: desc (ANSI) TODO: portarlo a utf-8
         for row, badge in enumerate(self.badges):
             uuid, nome, url, _ = badge
-            item = QTableWidgetItem(QIcon(self.getBadgeIcon(uuid, url, "_64")), nome)
+            item = QTableWidgetItem(
+                QIcon(self.getBadgeIcon(uuid, url, "_64")), nome)
             item.setData(Qt.UserRole, badge)
             self.tabellaBages.setItem(row, 0, item)
         self.tabellaBages.itemSelectionChanged.connect(self.showBadgesInfo)
-        
 
 
 if __name__ == "__main__":
